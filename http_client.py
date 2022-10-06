285390cccfea9ba2233c63bb01e7a1a94a1b03e4
Adam Stewart <adamjoesph11@gmail.com>
2022-10-05 19:27:56 -0500

    calls now work with http and www

--- http_client.py
+++ http_client.py
@@ -1,14 +1,20 @@
-import socket 
+import socket
 import sys
+from tempfile import SpooledTemporaryFile
 exitCode = '400'
-url = "www.example.com" #sys.argv[1]
+url = sys.argv[1]  # sys.argv[1]
 PORT = 80
 # STD out only gets body of response, not headers
 # any other message goes to STD err
 # listen and send over port 80
-
-
-#gotta build HTTP stuff ourselve from header on down
+if 'http://' in url:
+    split = url.split('/')
+    host = split[2]
+    spot = split[3]
+else:
+    host = url
+    spot = ''
+# gotta build HTTP stuff ourselve from header on down
 response = ''
 # with is easier try catch that auto closes sthings
 # AF_INET is address protocol family
@@ -16,11 +22,11 @@ print(url)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # SOCK_STREAM specifies we are using TCP
     #s.bind((url, PORT))
-    s.connect((url,PORT))
+    s.connect((host, PORT))
     #addr = s.getaddrinfo()
-    req = "GET / HTTP/1.1\r\nHost:{}\r\n\r\n".format(url)
-    s.send(req.encode('utf-8'))   
-    # while True:
+    req = "GET /{} HTTP/1.1\r\nHost:{}\r\n\r\n".format(spot, host)
+    s.send(req.encode('utf-8'))
+    # while True:g http://insecure.stevetarzia.com/basic.html u
     #     window = s.recv(4096)
     #     if len(window) == 0:
     #         break
