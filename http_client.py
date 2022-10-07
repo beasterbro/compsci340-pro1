866c6f22f5eb196a44ffc186702d904fc10dd07d
Adam Stewart <adamjoesph11@gmail.com>
2022-10-07 14:38:51 -0500

    basic double redirr now works now

--- http_client.py
+++ http_client.py
@@ -24,7 +24,9 @@ def getBody(resp):
 
 
 def getUrl(header):
-    return header.split('Location: ')[1].split(' ')[0].split('\n')[0]
+    for s in header.split('\n'):
+        if "Location: " in s:
+            return s[10:]
 
 
 def getStatusCode(header):
@@ -34,16 +36,20 @@ def getStatusCode(header):
 # with is easier try catch that auto closes sthings
 # AF_INET is address protocol family
 def makeRequest(url):
+    print("start: "+url)
     if 'http://' or 'https://'in url:
         split = url.split('/')
+        print(split)
         host = split[2]
         spot = split[3]
     else:
+       # print("else: "+url)
         host = url
         spot = ''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # SOCK_STREAM specifies we are using TCP
         #s.bind((url, PORT))
+        #print(host + '\n')
         s.connect((host, PORT))
         #addr = s.getaddrinfo()
         req = "GET /{} HTTP/1.1\r\nHost:{}\r\n\r\n".format(spot, host)
@@ -58,20 +64,22 @@ def makeRequest(url):
         tempResp = response.decode()
         header = getHeader(tempResp)
         responseCode = getStatusCode(header)
+        #print(header)
         if responseCode == '200':
             body = getBody(tempResp)
             print(body)
         elif responseCode == '301' or responseCode == '302':
-            sys.stderr.write("Redirected to: " + getUrl(header))
-            makeRequest(getUrl(header))
+            tempUrl = getUrl(header)
+            #sys.stderr.write("Redirected to: " + tempUrl)
+            print("Redirr: " +tempUrl)
+            makeRequest(tempUrl.split('\r')[0])
         else:
             sys.stderr.write("Error Connecting with response: " + responseCode)
-
-        print()
-        if response:
-            sys.exit(0)
-        else:
-            sys.exit(1)
+        if responseCode != '302':
+            if response:
+                sys.exit(0)
+            else:
+                sys.exit(1)
         # print(response.decode())
 
 makeRequest(URL)
