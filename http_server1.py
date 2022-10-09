f9678b12b1e449a41511ed259de792693802fb62
Adam Stewart <adamjoesph11@gmail.com>
2022-10-09 17:02:30 -0500

    fixed so only html and htm works

--- http_server1.py
+++ http_server1.py
@@ -4,16 +4,25 @@ import sys
 import time
 from urllib import response
 
-filename = 'rfc2616.html'
+filename = 'rfc2616'
 body = ''
 head ='';
 port = int(sys.argv[1])
-statusCode = ''
+responseCode = ''
+responseStatus = ''
 
 def getResponseCode(req):
-    if '.htm' or '.html' in req:
-        return 200
+    global responseStatus
+    split = req.split('HTTP/')
+    print(split[0])
+    if filename in split[0]:#TODO:fix to not return 200 when not .html
+        if ('.htm' or '.html') in split[0]:
+            return 200
+        else:#not .htm or .html
+            responseStatus = 'Forbidden'
+            return 403
     else:
+        responseStatus = 'Not Found'
         return 404
 
 def makeHeader(body):
@@ -23,36 +32,17 @@ def makeHeader(body):
         'Content-Length': len(body),
         'Connection': 'close',
     }
+    global responseCode
     response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
     response_version = 'HTTP/1.1'
-    response_status = '200'
-    response_status_text = 'OK' # this can be random
+    response_status = responseCode
+    response_status_text = responseStatus # this can be random
     # sending all this stuff
     r = '%s %s %s\r\n' % (response_version, response_status, response_status_text)
     r += response_headers_raw
     r+= '\r\n'
     return r
 
-    # response_headers = {
-    #     'Content-Type': 'text/html; encoding=utf8',
-    #     'Content-Length': len(msg),
-    #     'Connection': 'close',
-    # }
-
-    # response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
-
-    # response_proto = 'HTTP/1.1'
-    # response_status = '200'
-    # response_status_text = 'OK' # this can be random
-
-    # # sending all this stuff
-    # r = '%s %s %s\r\n' % (response_proto, response_status, response_status_text)
-    # c.send(r)
-    # c.send(response_headers_raw)
-    # c.send('\r\n') # to separate headers from body
-    # c.send(msg.encode(encoding="utf-8"))
-
-    # c.close()
 #     HTTP/1.1 200 OK
 # Date: Sun, 09 Oct 2022 20:28:24 GMT
 # Server: Apache/2.4.52 () OpenSSL/1.0.2k-fips PHP/5.4.16
@@ -75,22 +65,29 @@ def makeHeader(body):
     # write http header to connection socket and open the file and write its contnets to the connection socket
     # if don't have file makke HTTP error response (404 Not found) and write it to the socket
     # if it does exists bot not correct ending (htm or html) return 403 forbidden
-def hostFile(fname, p):
+def hostFile():#TODO: Somehow check the file they are requesting for
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # SOCK_STREAM specifies we are using TCP
         s.bind(('', port))#listen to all addresses
         s.listen()
         #print(conn)
+        global responseCode
         while True:
             conn, addr = s.accept()
             req = conn.recv(1024)
+            #print(req.decode())
             responseCode = getResponseCode(req.decode())
-            if responseCode==200:
-                f = open(filename,'r')
+            if responseCode == 200:
+                f = open(filename + '.html','r')
                 body = f.read()
                 head = makeHeader(body)
                 conn.send(head.encode(encoding="utf-8"))
                 conn.sendall(body.encode(encoding="utf-8"))#Send html response + header
                 conn.close()
-
-hostFile(filename,port)
+            else:#400 or the sort
+                body = 'RIP'
+                head = makeHeader(body)
+                conn.send(head.encode(encoding="utf-8"))
+                conn.sendall(body.encode(encoding="utf-8"))#Send html response + header
+                conn.close()
+hostFile()
